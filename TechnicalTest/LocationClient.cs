1a438f7959c103d558b24b8db080d202a6d3b3ac
Subbhashree Varatharajan <subbhashree07@gmail.com>
2023-07-08 22:26:15 +0100

    Updated step definitions for all the scenarios

--- TechnicalTest/LocationClient.cs
+++ TechnicalTest/LocationClient.cs
@@ -3,6 +3,7 @@ using NUnit.Framework;
 using RestSharp;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
 using TechnicalTest.Service;
 
@@ -23,11 +24,27 @@ namespace TechnicalTest
         public void GetLocationInformation(string countryCode, string postCode)
         {
             var url = _configurationProvider.GetUrl();
+            //Format the url with countrycode and postcode
+            url = String.Format(url, countryCode, postCode);
+            Console.WriteLine("Location url = " + url);
+            //Call get method to retrieve the location information
+            Response = _resrService.Get(url);
+            
         }
 
         public void VerifyRequestStatus(string isSuccessful)
         {
-
+            //assert the response status to Httpstatuscode
+            if (isSuccessful.Equals("true"))
+            {
+                Assert.That(Response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+                Console.WriteLine("Location is available as expected in zippopotam.us API");
+            }
+            else
+            {
+                Assert.That(Response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+                Console.WriteLine("Location is not found in zippopotam.us API");
+            }
         }
     }
 }
--- TechnicalTest/StepDefintion/LocationTestSteps.cs
+++ TechnicalTest/StepDefintion/LocationTestSteps.cs
@@ -8,6 +8,7 @@ namespace TechnicalTest
     public class LocationTestSteps
     {
         private readonly LocationClient _locationClient;
+        
         public LocationTestSteps(LocationClient locationClient)
         {
             _locationClient = locationClient;
@@ -16,12 +17,15 @@ namespace TechnicalTest
         [Given(@"I make a request to get location information (.*),(.*)")]
         public void GivenIMakeARequestToGetLocationInformation(string countryCode, string postCode)
         {
-            
+            //call GetLocationMethod method to make a call to URL and retrieve response
+            _locationClient.GetLocationInformation(countryCode, postCode);
         }
         
         [Then(@"I verify the request status (.*)")]
         public void ThenTheRequestShouldBeSuccessful(string isSuccessful)
-        {           
+        {
+            //call verifyRequestStatus method to assert the request status
+            _locationClient.VerifyRequestStatus(isSuccessful);
 
         }
     }
